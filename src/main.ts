db25d4631c8503b14b8367db2645e5da3648c73a
Christopher LaPonsie <claponsie2595@kvcc.edu>
2023-01-22 01:56:48 -0500

    Both left and right flippers are working

--- src/main.ts
+++ src/main.ts
@@ -38,11 +38,13 @@ async function loadSvg(
   return bodies;
 }
 
-var paddleDown = false;
+var paddleLDown = false;
+var paddleRDown = false;
 
-async function buildFlipper(x: number, y: number): Promise<Matter.Composite> {
-  const bodies = await loadSvg("./flipper.svg", x, y);
-
+async function buildLFlipper(x: number, y: number): Promise<Matter.Composite> {
+  const bodies = await loadSvg("./flipperL.svg", x, y);
+  console.log("Loading left");
+  console.log(bodies);
   const c = Matter.Composite.create();
   var blockerBottom = Matter.Bodies.rectangle(x + 12, y + 45, 105, 35, {
     isStatic: true,
@@ -103,9 +105,12 @@ async function buildFlipper(x: number, y: number): Promise<Matter.Composite> {
     plugin: {
       attractors: [
         function (bodyA: Matter.Body, bodyB: Matter.Body) {
+          if (bodyA != bodies[0] && bodyB != bodies[0]) {
+            return;
+          }
           var force = {
-            x: (bodyA.position.x - bodyB.position.x) * 0.001,
-            y: (bodyA.position.y - bodyB.position.y) * 0.001,
+            x: (bodyA.position.x - bodyB.position.x) * 0.002,
+            y: (bodyA.position.y - bodyB.position.y) * 0.002,
           };
           // apply force to both bodies
           Matter.Body.applyForce(
@@ -113,7 +118,7 @@ async function buildFlipper(x: number, y: number): Promise<Matter.Composite> {
             bodyA.position,
             Matter.Vector.neg(force)
           );
-          if (paddleDown) {
+          if (paddleLDown) {
             Matter.Body.applyForce(bodyB, bodyB.position, force);
           }
         },
@@ -124,6 +129,102 @@ async function buildFlipper(x: number, y: number): Promise<Matter.Composite> {
       visible: true,
       fillStyle: "red",
     },
+  });
+  Matter.Composite.add(c, flipper);
+  Matter.Composite.add(c, body);
+  Matter.Composite.add(c, [blockerBottom, blockerTop]);
+  return c;
+}
+
+async function buildRFlipper(x: number, y: number): Promise<Matter.Composite> {
+  const bodies = await loadSvg("./a.svg", x, y);
+  console.log("Loading right");
+  console.log(bodies);
+  const c = Matter.Composite.create();
+  var blockerBottom = Matter.Bodies.rectangle(x - 12, y + 45, 105, 35, {
+    isStatic: true,
+    render: {},
+    collisionFilter: {
+      category: 0x0010, // special category just for flipper
+      mask: 0x1010,
+    },
+  });
+  const blockerTop = Matter.Bodies.rectangle(x - 20, y - 55, 105, 55, {
+    isStatic: true,
+    render: {
+      strokeStyle: "#333333",
+      fillStyle: "blue",
+    },
+    collisionFilter: {
+      category: 0x010, // special category just for flipper
+      mask: 0x1010,
+    },
+  });
+
+  // flipper constraints
+
+  bodies.forEach((b) => {
+    b.collisionFilter.category = 0x1000;
+    b.collisionFilter.mask = 0x1111; // collides with anything
+  });
+
+  var body = Matter.Bodies.circle(x - 30, y - 70, 35, {
+    plugin: {
+      attractors: [
+        function (bodyA: Matter.Body, bodyB: Matter.Body) {
+          if (bodyA != bodies[0] && bodyB != bodies[0]) {
+            return;
+          }
+          var force = {
+            x: (bodyA.position.x - bodyB.position.x) * 0.002,
+            y: (bodyA.position.y - bodyB.position.y) * 0.002,
+          };
+          // apply force to both bodies
+          Matter.Body.applyForce(
+            bodyA,
+            bodyA.position,
+            Matter.Vector.neg(force)
+          );
+          if (paddleRDown) {
+            Matter.Body.applyForce(bodyB, bodyB.position, force);
+          }
+        },
+      ],
+    },
+    isStatic: true,
+    render: {
+      visible: true,
+      fillStyle: "red",
+    },
+  });
+  var pivotConstraint = Matter.Constraint.create({
+    pointA: { x: x + 12, y: y },
+    bodyB: bodies[0],
+    pointB: { x: 12.8, y: 0.0 },
+    stiffness: 1,
+    length: 0,
+    render: {
+      visible: true,
+      type: "line",
+    },
+  });
+
+  var springConstraint = Matter.Constraint.create({
+    bodyA: bodies[0],
+    pointA: { x: -22, y: 0 },
+    bodyB: blockerBottom,
+    pointB: { x: -0, y: 0 },
+    stiffness: 0.02,
+    length: 0,
+    render: {
+      visible: true,
+      strokeStyle: "green",
+    },
+  });
+
+  var flipper = Matter.Composite.create({
+    bodies: bodies,
+    constraints: [pivotConstraint, springConstraint],
   });
   Matter.Composite.add(c, flipper);
   Matter.Composite.add(c, body);
@@ -155,8 +256,10 @@ window.addEventListener("load", async () => {
   // load svg files
 
   // build flipper
-  const flipper = await buildFlipper(200, 200);
-  Matter.World.add(engine.world, flipper);
+  const flipperL = await buildLFlipper(200, 200);
+  Matter.World.add(engine.world, flipperL);
+  const flipperR = await buildRFlipper(400, 200);
+  Matter.World.add(engine.world, flipperR);
 
   // run the renderer
   Matter.Render.run(render);
@@ -171,7 +274,10 @@ window.addEventListener("load", async () => {
   // on keydown
   window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowLeft") {
-      paddleDown = true;
+      paddleLDown = true;
+    }
+    if (e.key === "ArrowRight") {
+      paddleRDown = true;
     }
   });
 
@@ -179,7 +285,10 @@ window.addEventListener("load", async () => {
 
   window.addEventListener("keyup", (e) => {
     if (e.key === "ArrowLeft") {
-      paddleDown = false;
+      paddleLDown = false;
+    }
+    if (e.key === "ArrowRight") {
+      paddleRDown = false;
     }
   });
 });
